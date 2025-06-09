ef4f05691a5440e227e95b74f63f6f9cfd874ccc
unknown <mathujaparameshwaran@gmail.com>
2025-06-09 15:58:05 +0530

    m

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    _id: { type: String, required: true }, // Clerk User ID
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    image: { type: String, required: false },
-    role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
-    recentSearchedCities: { type: [String], default: [] }, // ✅ Now optional with default empty array
-}, { timestamps: true });
+const userSchema = mongoose.Schema({
+    _id: {type: String, required: true},
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    image: {type: String, required: false},
+    role: {type: String, enum: ["user", "hotelOwner"], default: "user"},
+   recentSearchedCities: { type: [String], default: [] },
 
-const User = mongoose.model("User", userSchema);
+    },{timestamps: true}
+);
+
+const User = new mongoose.model("User", userSchema);
 
 export default User;
