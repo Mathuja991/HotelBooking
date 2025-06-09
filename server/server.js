ae39fa5dcef95d784f80528b11532eb3030cb238
unknown <mathujaparameshwaran@gmail.com>
2025-06-09 22:56:41 +0530

    lag

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.use(clerkMiddleware())
 app.use(express.json())
 
 // Add a raw body parser only for the webhook route
-app.post('/api/clerk', bodyParser.raw({ type: '*/*' }), clerkWebhooks)
+app.post("/api/webhooks/clerk", express.raw({ type: 'application/json' }), clerkWebhooks);
+
 
 app.get('/', (req, res) => res.send("API is working"))
 
