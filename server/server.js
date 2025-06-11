7442821d8c9c8a085bc3f258ad79a61a9c77b890
unknown <mathujaparameshwaran@gmail.com>
2025-06-11 16:28:39 +0530

    lj

--- server/server.js
+++ server/server.js
@@ -5,8 +5,14 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controller/clerkWebhooks.js";
 import bodyParser from 'body-parser'
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 connectDB()
+connectCloudinary();
 const app = express()
 
 app.use(cors()) // Enable Cross-Origin Resource Sharing
@@ -19,6 +25,10 @@ app.use(clerkMiddleware())
 app.post('/api/clerk', bodyParser.raw({ type: '*/*' }), clerkWebhooks);
 app.use(express.json());
 app.get('/', (req, res) => res.send("API is working"))
+app.use('api/user',userRouter)
+app.use('api/hotels',hotelRouter)
+app.use('api/rooms',roomRouter)
+app.use('api/bookings',bookingRouter)
 
 const PORT = process.env.PORT || 3000;
 
