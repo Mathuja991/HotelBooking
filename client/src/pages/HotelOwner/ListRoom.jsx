3d93e6ca25584ed49aa9d7321a08fce21394fb68
unknown <mathujaparameshwaran@gmail.com>
2025-06-12 17:05:44 +0530

    j

--- client/src/pages/HotelOwner/ListRoom.jsx
+++ client/src/pages/HotelOwner/ListRoom.jsx
@@ -1,9 +1,53 @@
-import React, { useState } from 'react';
-import { roomsDummyData } from '../../assets/assets';
+import React, { useEffect, useState } from 'react';
+//import { roomsDummyData } from '../../assets/assets';
 import Title from '../../components/Title';
+import { useAppContext } from '../../context/AppContext';
 
 const ListRoom = () => {
-  const [rooms, setRooms] = useState(roomsDummyData);
+
+
+const [rooms, setRooms] = useState([])
+const {axios, getToken, user} = useAppContext()
+
+// Fetch Rooms of the Hotel Owner
+const fetchRooms = async ()=>{
+    try {
+
+      const { data } = await axios.get('/api/rooms/owner', {headers:
+        {Authorization : `Bearer ${await getToken()}`}
+      })
+   
+    if (data.success){
+        setRooms(data.rooms)
+    
+    }else{
+      toast.error(data.message)
+    }
+
+    }catch(error){
+       toast.error(error.message)
+    }
+    
+  }
+    const toggleAyailability = async (roomId)=>{
+      const {data} = await axios.post('/api/rooms/toggle-availability', {roomId},
+      {headers: {Authorization: `Bearer ${await getToken()} `}})
+      if (data.success) {
+         toast.success(data.message)
+         fetchRooms()
+      }else{
+         toast.error(data.message)
+      }
+
+
+      useEffect(()=>{
+          if(user){
+          fetchRooms()
+          }
+          }, [user])
+
+          
+        }
 
   return (
     <div className="p-4 max-w-6xl mx-auto">
@@ -36,6 +80,7 @@ const ListRoom = () => {
                 <td className="px-6 py-4 text-center">
                   <label className="inline-flex items-center cursor-pointer relative">
                     <input
+                      onChange={()=> toggleAyailability(item._id)}
                       type="checkbox"
                       className="sr-only peer"
                       checked={item.isAvailable}
--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -6,7 +6,15 @@ const connectCloudinary = () => {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+ 
+
+    
+   });
+   console.log("Cloudinary Config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "******" : "NOT SET",
+});
 };
 
 export default connectCloudinary;
