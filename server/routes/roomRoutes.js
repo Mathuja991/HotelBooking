60efa96453988fd427b84eb9cc3cb854ac24bed8
unknown <mathujaparameshwaran@gmail.com>
2025-06-13 13:50:27 +0530

    lal

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { createRoom, getOwnerrooms, getRooms, toggleRoomAvailability } from "../controller/roomController.js";
+import { createRoom, getOwnerrooms, getRooms, toggleRoomAvailability,deleteRoom  } from "../controller/roomController.js";
 
 
 const roomRouter =express.Router();
@@ -9,7 +9,7 @@ roomRouter.post('/',upload.array("images",4),protect,createRoom)
 roomRouter.get('/',getRooms)
 roomRouter.get('/owner',protect,getOwnerrooms)
 roomRouter.post('/toggle-availability',protect,toggleRoomAvailability)
-
+roomRouter.delete('/:roomId', protect, deleteRoom)
 
 
 export default roomRouter;
