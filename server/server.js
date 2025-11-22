5ab4393e25ecedf93ccded5fe0abe473ab4c2a15
unknown <mathujaparameshwaran@gmail.com>
2025-11-22 21:34:08 +0530

    Name1

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ connectCloudinary();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // ✅ Your frontend URL
+    origin: '*', // ✅ Your frontend URL
     credentials: true // ✅ Allow cookies, auth headers, etc.
 }));
 
